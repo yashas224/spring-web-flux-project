2d2d7222f144a7ff3ac331b5b156fd9a35177eee
yashas224 <71977329+yashas224@users.noreply.github.com>
2022-02-01 22:10:53 +0530

    Update FluxAndMonoControllerTest.java
    Different Testing approaches for reactive Controllers

--- reactive-spring-webflux/movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
+++ reactive-spring-webflux/movies-info-service/src/test/java/unit/com/reactivespring/controller/FluxAndMonoControllerTest.java
@@ -1,14 +1,99 @@
 package com.reactivespring.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-@WebFluxTest
+@WebFluxTest(FluxAndMonoController.class)
 class FluxAndMonoControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
 
     @Test
     void getFlux() {
+
+        webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .hasSize(4);
+    }
+
+    @Test
+    void getFlux_approach2() {
+
+        Flux<Integer> result = webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(result)
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void getFlux_approach3() {
+
+        webTestClient
+                .get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(Integer.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    var response = listEntityExchangeResult.getResponseBody();
+                    Assertions.assertEquals(4, response.size());
+                });
+    }
+
+
+    @Test
+    void mono() {
+        webTestClient
+                .get()
+                .uri("/mono")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(String.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    var response = listEntityExchangeResult.getResponseBody();
+                    Assertions.assertEquals("Hello-world", response);
+                });
+    }
+
+
+    @Test
+    void SSE() {
+
+        Flux<Long> result = webTestClient
+                .get()
+                .uri("/stream")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Long.class)
+                .getResponseBody();
+
+        StepVerifier.create(result)
+                .expectNext(0l, 1l, 2l)
+                .thenCancel()
+                .verify();
     }
 }
